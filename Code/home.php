e6666e60c2575caacc08568e0c3208d4943c329e
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-01 14:51:50 +0100

    insertion du formulaire dans la page et ajout de la gallerie photo

--- Code/home.php
+++ Code/home.php
@@ -16,6 +16,48 @@
         include "nav.php";
         ?>
     </div>
+    <div class="main">
+        <div style="width:auto;" id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1>Bienvenue chez <i>Restaurant Baratie</i></h1>
+                        <p>Da place 2b 4 ur food</p>
+                    </div>
+                    <img src="./img/restaurant1.jpg" class="d-block w-100" alt="restaurant">
+                </div>
+                <div class="carousel-item">
+                    <img src="./img/wine.jpg" class="d-block w-100" alt="vin">
+                    <div class="carousel-caption d-none d-md-block position-absolute top-0 start-50 translate-middle-x col-10 col-sm-10 col-md-10">
+                        <h1>Notre cave à vin est l'endroit idéal pour se mettre une sombre mine</h1>
+                        <p>Da place 2b 4 ur food</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="./img/salon.jpg" class="d-block w-100" alt="salon">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <div id="contactForm">
+        <?php
+            include 'formContact.php'
+        ?>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
 
