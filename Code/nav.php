75ddaefa2d491b6330a5061f416d9c890820cd76
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-01 14:52:15 +0100

    modif de couleur pour la navigation

--- Code/nav.php
+++ Code/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="home.php">
             <img src="../logoBaratie.png" alt="logo" width="100" height="100" class="d-inline-block align-top">
@@ -11,7 +11,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="font-size: 20px;">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="home.php">Page d'accueil</a>
+                    <a class="nav-link" aria-current="page" href="home.php">Page d'accueil</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="menu.php">Menu</a>
