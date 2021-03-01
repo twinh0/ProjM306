5432f4634906832581f75a0bac02d0332e905c34
Luca <luca.rmn2@eduge.ch>
2021-03-01 13:33:53 +0100

    ajout message transaction effectuée

--- Code/transaction.php
+++ Code/transaction.php
@@ -15,6 +15,37 @@
         <?php
         include "nav.php";
         ?>
+        <?php
+        $validOuNon = true;
+        // vérification de la validation de la transaction (simulation)
+        // Verification passée
+        if ($validOuNon == true) {
+        ?>
+            <main>
+                <section class="container-fluid jumbotron mt-5">
+                    <section class="row justify-content-center">
+                        <section class="col-6 col-sm-6 col-md-6 bg-dark text-white p-5 border border-dark rounded">
+                            <div class="container">
+                                <div id="quiz">
+                                    <h1 class="text-success text-center">Paiement effectué</h1>
+
+                                    <h4 class="text-center">Votre paiement à été effectué avec succès ! votre commende va bientôt être prise en charge.</h4>
+
+                                </div>
+                            </div>
+                        </section>
+                    </section>
+                </section>
+            </main>
+        <?php
+        }
+        // Verification pas passée
+        else {
+        ?>
+
+        <?php
+        }
+        ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 </body>
