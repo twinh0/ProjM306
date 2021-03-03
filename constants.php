a94cf4984fae2d95c2c97508b05726ffa1f0a818
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-03 13:17:35 +0100

    ajout du fichier qui définit les chemins vers les pages du site

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+
+define('ACCUEIL', 'views/home.php');
+define('MENU', 'views/menu.php');
+define('CONNEXION', 'views/connexion.php');
+define('PANIER', 'views/panier.php');
+define('ERROR', 'views/404.php');
+
+?>
