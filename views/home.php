746eee8f7468d5d6c25dcc6c062c17636108c587
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-03 13:19:18 +0100

    arborescence retravaillée

--- views/home.php
+++ views/home.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link href="styles/style.css" rel="stylesheet">
+    <link href="./styles/style.css" rel="stylesheet">
     <title>Page d'accueil</title>
 </head>
 
@@ -32,17 +32,17 @@
                         <h1 style="font-family: 'Dancing Script', cursive; font-size: 3em;">Bienvenue chez <i>Restaurant Baratie</i></h1>
                         <p style="font-family: 'Dancing Script', cursive; font-size: 1em;">Da place 2b 4 ur food</p>
                     </div>
-                    <img src="./img/restaurant1.jpg" class="d-block w-100" alt="restaurant">
+                    <img src="./ressources/restaurant1.jpg" class="d-block w-100" alt="restaurant">
                 </div>
                 <div class="carousel-item">
-                    <img src="./img/wine.jpg" class="d-block w-100" alt="vin">
+                    <img src="./ressources/wine.jpg" class="d-block w-100" alt="vin">
                     <div class="carousel-caption d-none d-md-block position-absolute top-0 start-50 translate-middle-x col-10 col-sm-10 col-md-10">
                         <h1 style="font-family: 'Dancing Script', cursive; font-size: 3em;">Notre cave à vin est l'endroit idéal pour se mettre une sombre mine</h1>
                         <p style="font-family: 'Dancing Script', cursive; font-size: 1em;">Da place 2b 4 ur food</p>
                     </div>
                 </div>
                 <div class="carousel-item">
-                    <img src="./img/salon.jpg" class="d-block w-100" alt="salon">
+                    <img src="./ressources/salon.jpg" class="d-block w-100" alt="salon">
                 </div>
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -62,6 +62,10 @@
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+
+    <!-- reference to Google Authentication  -->
+    <!-- <script src="https://apis.google.com/js/platform.js" async defer></script> -->
 </body>
 
 </html>
--- views/nav.php
+++ views/nav.php
@@ -1,31 +1,32 @@
+<?php
+session_start();
+?>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
 </style>
-
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="home.php">
-            <img src="../logoBaratie.png" alt="logo" width="100" height="100" class="d-inline-block align-top">
+        <a class="navbar-brand" href="./index.php?action=accueil">
+            <img src="./ressources/logoBaratie.png" alt="logo" width="100" height="100" class="d-inline-block align-top">
             <div style="display: inline; font-size: 3em; font-family: 'Dancing Script', cursive;"><i>Baratie</i></div>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="font-size: 20px;">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="home.php">Page d'accueil</a>
+                    <a class="nav-link" name="choice" value="home" href="./index.php?action=accueil">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="menu.php">Menu</a>
+                    <a class="nav-link" name="choice" value="menu" href="./index.php?action=menu">Menu</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="connexion.php">Connexion/Inscription</a>
+                    <a class="nav-link" name="choice" value="connexion" href="./index.php?action=connexion">Connexion</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="panier.php">Panier</a>
+                    <a class="nav-link" name="choice" value="panier" href="./index.php?action=panier">Panier</a>
                 </li>
             </ul>
         </div>
