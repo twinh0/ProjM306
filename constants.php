54cd950a850b9e64fbbad86b1889cdeacbd3c850
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-09 16:42:40 +0100

    ajout de la constante CREATIONCOMPTE

--- constants.php
+++ constants.php
@@ -4,6 +4,7 @@ define('ACCUEIL', 'views/home.php');
 define('MENU', 'views/menu.php');
 define('CONNEXION', 'views/connexion.php');
 define('PANIER', 'views/panier.php');
-define('ERROR', 'views/404.php');
+define('CREATIONCOMPTE', 'views/compteConfirmation.php')
+
 
 ?>
