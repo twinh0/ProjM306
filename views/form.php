2fee46049047d67f927aa93280ca91224b94ab15
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-09 16:46:05 +0100

    création d'un compte utilisateur finalisée

--- views/form.php
+++ views/form.php
@@ -1,12 +1,15 @@
 <?php
 require_once('./database/header.inc.php');
 
-$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-$prenom = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
-$nom = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
-$numTel = filter_input(INPUT_POST, 'numTel', FILTER_SANITIZE_STRING);
-$age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
-$mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_STRING);
+#region Deprecated Code
+// $username = filter_input(INPUT_POST, FILTER_SANITIZE_STRING);
+// $prenom = filter_input(INPUT_POST, 'fname', FILTER_SANITIZE_STRING);
+// $nom = filter_input(INPUT_POST, 'lname', FILTER_SANITIZE_STRING);
+// $numTel = filter_input(INPUT_POST, 'numTel', FILTER_SANITIZE_STRING);
+// $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_NUMBER_INT);
+// $mdp = filter_input(INPUT_POST, 'mdp', FILTER_SANITIZE_STRING);
+#endregion
+
 $seConnecter = filter_input(INPUT_POST, 'valider');
 
 //REGEX validation
@@ -14,141 +17,184 @@ $regexAlphabet = '/^[a-zA-Z ]*$/';
 $regexAlphaNumeric = '/[a-zA-Z0-9]+$/';
 $regexNumeric = '/^[0-9]+$/';
 $regexEmail = '/([\w\-]+\@[\w\-]+\.[\w\-]+)/';
-$regexNumTel = '/(\b(0041|0)|\B\+41)(\s?\(0\))?(\s)?[1-9]{2}(\s)?[0-9]{3}(\s)?[0-9]{2}(\s)?[0-9]{2}\b/';
+
+
+$regexNumTel = '/(\+41)\s(\d{2})\s(\d{3})\s(\d{2})\s(\d{2})/';
+
 //At least 8 characters, must contain at least 1 number, 1 uppercase character and 1 lowercase character
 $regexMdp = '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/';
 
 //Output for errors
-$usernameError = "";
-$prenomError = "";
-$nomError = "";
-$ageError = "";
-$emailError = "";
-$numTelError = "";
-$mdpError = "";
+$globalError = "Votre formulaire a été mal renseigné. Veuiller réessayer à nouveau!";
 
+
+//Tableau des variables non validées
+$nonValidatedVarsArray = $_POST;
+unset($nonValidatedVarsArray['valider']);
+
+//Tableau des variables validées
+$validatedVarsArray = array();
+
+//Affichage du formulaire d'inscription ou connexion en fonction de la valeur de $accountExists
+$accountExists = filter_input(INPUT_GET, 'accountExists', FILTER_SANITIZE_STRING);
+
+/**
+ * Script pour la création d'un compte + vérification si le compte existe
+ */
 if ($seConnecter) {
 
-    if (empty($_POST['username'])) {
-        $usernameError = "Veuillez rentrer un nom d'utilisateur";
-    } else {
+    if (count($_POST) == 8) {
 
-        if (preg_match($regexAlphaNumeric, $_POST['username'])) {
-            $username = $_POST['username'];
-        } else {
-            $usernameError = "Le nom d'utilisateur doit comporter uniquement des lettres et/ou des chiffres";
+        foreach ($nonValidatedVarsArray as $key => $value) {
+
+            switch ($key) {
+
+                case 'username':
+                    if (preg_match($regexAlphaNumeric, $_POST['username'])) {
+                        $username = $_POST['username'];
+
+                        array_push($validatedVarsArray, $username);
+                    }
+                    break;
+                case 'fname':
+                    if (preg_match($regexAlphabet, $_POST['fname'])) {
+                        $prenom = $_POST['fname'];
+
+                        array_push($validatedVarsArray, $prenom);
+                    }
+                    break;
+                case 'lname':
+                    if (preg_match($regexAlphabet, $_POST['lname'])) {
+                        $nom = $_POST['lname'];
+
+                        array_push($validatedVarsArray, $nom);
+                    }
+                    break;
+                case 'age':
+                    if (preg_match($regexNumeric, $_POST['age'])) {
+                        $age = $_POST['age'];
+
+                        array_push($validatedVarsArray, $age);
+                    }
+                    break;
+                case 'email':
+                    if (preg_match($regexEmail, $_POST['email'])) {
+                        $email = $_POST['email'];
+
+                        array_push($validatedVarsArray, $email);
+                    }
+                    break;
+                case 'numTel':
+                    if (preg_match($regexNumTel, $_POST['numTel'])) {
+                        $numTel = $_POST['numTel'];
+
+                        array_push($validatedVarsArray, $numTel);
+                    }
+                    break;
+                case 'mdp':
+                    if (preg_match($regexMdp, $_POST['mdp'])) {
+                        $mdp = md5($_POST['mdp']);
+
+                        array_push($validatedVarsArray, $mdp);
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
-    if(empty($_POST['prenom'])){
-        $prenomError = "Veuillez renseigner votre prénom";
-    }else{
-
-        if(preg_match($regexAlphabet, $_POST['prenom'])){
-            $prenom = $_POST['prenom'];
-        }else{
-            $prenomError = "Votre prénom doit contenir uniquement des lettres";
-        }
-    }
-
-    if(empty($_POST['nom'])){
-        $prenomError = "Veuillez renseigner votre prénom";
-    }else{
-
-        if(preg_match($regexAlphabet, $_POST['nom'])){
-            $prenom = $_POST['nom'];
-        }else{
-            $prenomError = "Votre nom doit contenir uniquement des lettres";
-        }
-    }
-
-    if(empty($_POST['age'])){
-        $ageError = "Veuillez renseigner votre âge";
-    }else{
-
-        if(preg_match($regexNumeric, $_POST['age'])){
-            $age = $_POST['age'];
-        }else{
-            $ageError = "Votre âge doit contenir uniquement des chiffres";
-        }
-    }
-
-
-    if(empty($_POST['email'])){
-        $emailError = "Veuillez renseigner votre email";
-    }else{
-
-        if(preg_match($regexEmail, $_POST['email'])){
-            $email = $_POST['email'];
-        }else{
-            $emailError = "Votre email est incorrect";
-        }
-    }
-
-    if(empty($_POST['numTel'])){
-        $prenomError = "Veuillez renseigner votre numéro de téléphone";
-    }else{
-
-        if(preg_match($regexNumTel, $_POST['numTel'])){
-            $numTel = $_POST['numTel'];
-        }else{
-            $prenomError = "Votre numéro de téléphone doit correspondre au format : + 41 XX XXX XX XX";
-        }
-    }
-
-    if (empty($_POST['mdp'])) {
-
-        $mdpError = "Veuillez renseigner votre mot de passe";
-    } else {
-
-        if (preg_match($regexMdp, $_POST['mdp'])) {
-            $mdp = $_POST['mdp'];
-        } else {
-            $mdpError = "Minimum 8 caractères, minimum 1 chiffre, une majuscule et une minuscule";
-        }
-        
-    }
-
-
-    if ($username != null && $prenom != null && $nom != null && $email != null && $age != null) {
+    if (count($validatedVarsArray) == count($nonValidatedVarsArray)) {
 
         try {
-            //Requête préparée
-            $insertNewUser = $bdd->prepare("INSERT INTO utilisateur (prenom, nom, age, numTel, email, mdp) VALUES (:prenom, :nom, :age, :numTel, :email, :mdp)");
-            $insertNewUser->bindParam(':prenom', $prenom);
-            $insertNewUser->bindParam(':nom', $nom);
-            $insertNewUser->bindParam(':age', $age);
-            $insertNewUser->bindParam(':numTel', $numTel);
-            $insertNewUser->bindParam(':email', $email);
-            $insertNewUser->bindParam(':mdp', $mdp);
 
-            //Execution de la requête
-            $insertNewUser->execute();
+            $checkIfUserExists = $bdd->prepare("SELECT * FROM utilisateur WHERE nomUtilisateur = :nomUtilisateur OR email = :email");
+            $checkIfUserExists->bindParam(':nomUtilisateur', $username);
+            $checkIfUserExists->bindParam(':email', $email);
+
+            $checkIfUserExists->execute();
+
+            $resultatCheckUser = $checkIfUserExists->fetchAll(PDO::FETCH_ASSOC);
+
+            if (empty($resultatCheckUser)) {
+                //Requête préparée
+                $insertNewUser = $bdd->prepare("INSERT INTO utilisateur (nomUtilisateur, prenom, nom, age, numTel, email, mdp) VALUES (:nomUtilisateur, :prenom, :nom, :age, :numTel, :email, :mdp)");
+                $insertNewUser->bindParam(':nomUtilisateur', $username);
+                $insertNewUser->bindParam(':prenom', $prenom);
+                $insertNewUser->bindParam(':nom', $nom);
+                $insertNewUser->bindParam(':age', $age);
+                $insertNewUser->bindParam(':numTel', $numTel);
+                $insertNewUser->bindParam(':email', $email);
+                $insertNewUser->bindParam(':mdp', $mdp);
+
+                //Execution de la requête
+                $insertNewUser->execute();
+                
+                header('Location: ./index.php?action=accountCreated');
+
+            } else {
+                echo '<div class="container-fluid d-flex justify-content-center align-items-center h-100">';
+                echo '<div id="errorOutput" class="alert alert-danger alert-dismissible fade show" role="alert">';
+                echo "Un compte existe déjà pour ce nom d'utilisateur/email";
+                echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+                echo '</div>';
+                echo '</div>';
+            }
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
+    } else {
+        echo '<div class="container-fluid d-flex justify-content-center align-items-center h-100">';
+        echo '<div id="errorOutput" class="alert alert-danger alert-dismissible fade show" role="alert">';
+        echo $globalError;
+        echo '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+        echo '</div>';
+        echo '</div>';
     }
 }
-
 ?>
 
-<div class="body-background">
-    <div class="container-fluid d-flex justify-content-center align-items-center h-100">
-        <div class="card p-3 text-center py-4">
-            <form method="POST" action="#">
-                <h4>Create account</h4>
-                <div> <span>Already have an account?</span> <a href="#" class="text-decoration-none">Signin</a> </div>
-                <div class="mt-3 px-3"> <input type="text" name="username" class="form-control" placeholder="Nom d'utilisateur" required> </div>
-                <div class="input-group px-3 mt-3"> <input type="text" class="form-control" name="fname" placeholder="Prénom" aria-label="Username" required> <span></span> <input type="text" class="form-control" name="lname" placeholder="Nom" aria-label="Server" required> </div>
-                <div class="mt-3 px-3"> <input type="number" name="age" placeholder="Votre âge" class="form-control" min="18" required /> </div>
-                <div class="mt-3 px-3"> <input type="email" name="email" placeholder="Votre email" class="form-control" required /> </div>
-                <div class="mt-3 px-3"> <input type="tel" name="numTel" class="form-control" placeholder="Numéro de téléphone, format: +41 XX XXX XX XX" pattern="/(\b(0041|0)|\B\+41)(\s?\(0\))?(\s)?[1-9]{2}(\s)?[0-9]{3}(\s)?[0-9]{2}(\s)?[0-9]{2}\b/" required> </div>
-                <div class="mt-3 px-3"> <input type="password" name="mdp" placeholder="Votre mot de passe" class="form-control" required /> </div>
-                <div class="mt-3 d-grid px-3"> <input type="submit" name="valider" value="Se connecter" class="btn btn-primary btn-block btn-signup text-uppercase" />
-                    <div class="px-3">
-                        <!-- <div class="mt-2 form-check d-flex flex-row"> <input class="form-check-input" type="checkbox" value="" id="services"> <label class="form-check-label ms-2" for="services"> I have read and agree to the terms. </label> </div> -->
-                    </div>
-            </form>
+<?php if (filter_var($accountExists, FILTER_VALIDATE_BOOLEAN)) : ?>
+    <div class="body-background">
+        <div class="container-fluid d-flex justify-content-center align-items-center h-100">
+            <div class="card p-3 text-center py-4">
+                <form method="POST" action="#">
+                    <h4>Je possède un compte</h4>
+                    <div class="mt-3 px-3"> <input type="text" name="usernameLogin" class="form-control" placeholder="Nom d'utilisateur" required> </div>
+                    <div class="mt-3 px-3"> <input type="password" name="mdpLogin" placeholder="Votre mot de passe" class="form-control" required /> </div>
+                    <div class="mt-3 d-grid px-3"> <input type="submit" name="valider" value="Se connecter" class="btn btn-primary btn-block btn-signup text-uppercase" />
+                </form>
+            </div>
         </div>
     </div>
-</div>
+<?php else : ?>
+    <div class="body-background">
+        <div class="container-fluid d-flex justify-content-center align-items-center h-100">
+            <div class="card p-3 text-center py-4">
+                <form method="POST" action="#">
+                    <h4>Créer un compte</h4>
+                    <div> <span>Avez-vous un compte existant?</span> <a href="./index.php?action=connexion&accountExists=true" class="text-decoration-none">Me connecter</a> </div>
+                    <div class="mt-3 px-3"> <input type="text" name="username" class="form-control" placeholder="Nom d'utilisateur" value="<?php if (isset($username)) {
+                                                                                                                                                echo $username;
+                                                                                                                                            } ?>" required> </div>
+                    <div class="input-group px-3 mt-3"> <input type="text" class="form-control" name="fname" placeholder="Prénom" value="<?php if (isset($prenom)) {
+                                                                                                                                                echo $prenom;
+                                                                                                                                            } ?>" aria-label="Username" required><span></span> <input type="text" class="form-control" name="lname" placeholder="Nom" aria-label="Server" value="<?php if (isset($nom)) {
+                                                                                                                                                                                                                                                                                                        echo $nom;
+                                                                                                                                                                                                                                                                                                    } ?>" required> </div>
+                    <div class="mt-3 px-3"> <input type="number" name="age" placeholder="Votre âge" class="form-control" min="18" value="<?php if (isset($age)) {
+                                                                                                                                                echo $age;
+                                                                                                                                            } ?>" required /> </div>
+                    <div class="mt-3 px-3"> <input type="email" name="email" placeholder="Votre email" class="form-control" value="<?php if (isset($email)) {
+                                                                                                                                        echo $email;
+                                                                                                                                    } ?>" required /> </div>
+                    <div class="mt-3 px-3"> <input type="tel" name="numTel" class="form-control" placeholder="Numéro de téléphone, format: +41 XX XXX XX XX" value="<?php if (isset($numTel)) {
+                                                                                                                                                                        echo $numTel;
+                                                                                                                                                                    } ?>" required> </div>
+                    <div class="mt-3 px-3"> <input type="password" name="mdp" placeholder="Votre mot de passe" class="form-control" required /> </div>
+                    <div class="mt-3 d-grid px-3"> <input type="submit" id="createAccount" name="valider" value="Créer le compte" class="btn btn-primary btn-block btn-signup text-uppercase"/>
+                </form>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
