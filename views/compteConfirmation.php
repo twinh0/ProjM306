fa50ee87655eb4193aba0ebaa78e0c0dccc61884
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-09 16:44:16 +0100

    création de la page de confirmation de compte

---
+++ views/compteConfirmation.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link href="./styles/style.css" rel="stylesheet">
+    <title>Menu</title>
+</head>
+
+<body>
+    <div class="navigation">
+        <?php
+        include "nav.php";
+        ?>
+        <div id="affichage">
+            <p>Votre compte a été créé</p>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+
+</html>
