726402a94bfc601ba600fb9bea1492cecace76e1
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-10 10:35:54 +0100

    ajout de la constante pour la redirection vers la page de confirmation de la création du compte

--- constants.php
+++ constants.php
@@ -1,10 +1,11 @@
 <?php
 
+//Constantes nécessaire à la redirection
 define('ACCUEIL', 'views/home.php');
 define('MENU', 'views/menu.php');
 define('CONNEXION', 'views/connexion.php');
 define('PANIER', 'views/panier.php');
-define('CREATIONCOMPTE', 'views/compteConfirmation.php')
+define('CREATIONCOMPTE', 'views/compteConfirmation.php');
 
 
 ?>
