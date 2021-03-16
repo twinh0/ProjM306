735657720310ee085abc24e0eff2fcc12831f849
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-16 15:32:10 +0100

    définition des constantes pour les pages userPage et logout

--- constants.php
+++ constants.php
@@ -6,6 +6,8 @@ define('MENU', 'views/menu.php');
 define('CONNEXION', 'views/connexion.php');
 define('PANIER', 'views/panier.php');
 define('CREATIONCOMPTE', 'views/compteConfirmation.php');
+define('USER_PAGE', 'views/userPage.php');
+define('LOGOUT', 'views/logout.php');
 
 
 ?>
