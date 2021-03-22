a368b73a6a5005fc9c9548cdb70c71fc582a9007
Luca <luca.rmn2@eduge.ch>
2021-03-22 14:15:26 +0100

    changement des attributs et ajout de la méthode pour l'ajout

--- database/commande.php
+++ database/commande.php
@@ -5,93 +5,115 @@ class Commande
     #region Champs
 
     private $idCommande;
-    private $plat;
-    private $aConfirmer;
+    private $lstPlats;
+    private $estConfirme;
+    private $dateCommande;
     private $idUtilisateur;
 
     #endregion
 
     #region Propriétés
-
-    /**
-     * Get the value of idCommande
-     */
-    public function getIdCommande()
-    {
-        return $this->idCommande;
-    }
-
-    /**
-     * Set the value of idCommande
-     *
-     * @return  self
-     */
-    public function setIdCommande($idCommande)
-    {
-        $this->idCommande = $idCommande;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of plat
-     */
-    public function getPlat()
-    {
-        return $this->plat;
-    }
-
-    /**
-     * Set the value of plat
-     *
-     * @return  self
-     */
-    public function setPlat($plat)
-    {
-        $this->plat = $plat;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of aConfirmer
-     */
-    public function getAConfirmer()
-    {
-        return $this->aConfirmer;
-    }
-
-    /**
-     * Set the value of aConfirmer
-     *
-     * @return  self
-     */
-    public function setAConfirmer($aConfirmer)
-    {
-        $this->aConfirmer = $aConfirmer;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of idUtilisateur
-     */
-    public function getIdUtilisateur()
-    {
-        return $this->idUtilisateur;
-    }
-
-    /**
-     * Set the value of idUtilisateur
-     *
-     * @return  self
-     */
-    public function setIdUtilisateur($idUtilisateur)
-    {
-        $this->idUtilisateur = $idUtilisateur;
-
-        return $this;
-    }
+    
+        /**
+         * Get the value of idCommande
+         */ 
+        public function getIdCommande()
+        {
+            return $this->idCommande;
+        }
+    
+        /**
+         * Set the value of idCommande
+         *
+         * @return  self
+         */ 
+        public function setIdCommande($idCommande)
+        {
+            $this->idCommande = $idCommande;
+    
+            return $this;
+        }
+    
+        /**
+         * Get the value of lstPlats
+         */ 
+        public function getLstPlats()
+        {
+            return $this->lstPlats;
+        }
+    
+        /**
+         * Set the value of lstPlats
+         *
+         * @return  self
+         */ 
+        public function setLstPlats($lstPlats)
+        {
+            $this->lstPlats = $lstPlats;
+    
+            return $this;
+        }
+    
+        /**
+         * Get the value of estConfirme
+         */ 
+        public function getEstConfirme()
+        {
+            return $this->estConfirme;
+        }
+    
+        /**
+         * Set the value of estConfirme
+         *
+         * @return  self
+         */ 
+        public function setEstConfirme($estConfirme)
+        {
+            $this->estConfirme = $estConfirme;
+    
+            return $this;
+        }
+    
+        /**
+         * Get the value of dateCommande
+         */ 
+        public function getDateCommande()
+        {
+            return $this->dateCommande;
+        }
+    
+        /**
+         * Set the value of dateCommande
+         *
+         * @return  self
+         */ 
+        public function setDateCommande($dateCommande)
+        {
+            $this->dateCommande = $dateCommande;
+    
+            return $this;
+        }
+    
+        /**
+         * Get the value of idUtilisateur
+         */ 
+        public function getIdUtilisateur()
+        {
+            return $this->idUtilisateur;
+        }
+    
+        /**
+         * Set the value of idUtilisateur
+         *
+         * @return  self
+         */ 
+        public function setIdUtilisateur($idUtilisateur)
+        {
+            $this->idUtilisateur = $idUtilisateur;
+    
+            return $this;
+        }
+    
 
     #endregion
 
@@ -107,12 +129,26 @@ class Commande
     
     
     // }
+    public static function Add(Commande $uneCommande): bool
+    {
+        $request = ConnexionPdo::getInstance()->prepare("INSERT INTO commandes(lstPlats, estConfirme, dateCommande, idUtilisateur) VALUES (:lstPlats, :estConfirme, :dateCommande, :idUtilisateur)");
 
+        $lstPlats = $uneCommande->getLstPlats();
+        $estConfirme = $uneCommande->getEstConfirme();
+        $dateCommande = $uneCommande->getDateCommande();
+        $idUtilisateur = $uneCommande->getIdUtilisateur();
 
+        $request->bindParam(':lstPlats', $lstPlats);
+        $request->bindParam(':estConfirme', $estConfirme);
+        $request->bindParam(':dateCommande', $dateCommande);
+        $request->bindParam(':idUtilisateur', $idUtilisateur);
+
+        $insertionSuccessful = $request->execute();
+        return $insertionSuccessful;
+    }
 
 
 
     #endregion
-
 
 }
