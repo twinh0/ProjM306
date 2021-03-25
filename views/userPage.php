631b93e896acad5b4536c442c9e674cc8fcc43ad
iSar44 <saroukhanian.iliya@gmail.com>
2021-03-25 14:51:34 +0100

    affichage des commandes

--- views/userPage.php
+++ views/userPage.php
@@ -74,6 +74,30 @@ unset($displayInfo['mdp']);
                 ?>
             </table>
         </div>
+
+        <div>
+            <table class="table table-bordered table-striped">
+                <thead style="text-align:center;">
+                    <tr>
+                        <th>Date de la commande</th>
+                        <th>Liste des plats</th>
+                        <th>Prix Total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (!empty($userInfo)) {
+                        foreach ($infoCommande as $key => $value) {
+                            echo "<tr>";
+                            echo "<td>" . $infoCommande[$key]['dateCommande'] . "</td>";
+                            echo "<td>" . $infoCommande[$key]['lstPlats'] . "</td>";
+                            echo "</tr>";
+                        }
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
