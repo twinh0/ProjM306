c6ad57992ec4b29df6f0cadccb6ec8bec4282ce2
iSar44 <saroukhanian.iliya@gmail.com>
2021-04-12 11:01:48 +0200

    utilisation d'ìcones Bootstrap dans la nav

--- views/nav.php
+++ views/nav.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-
 if (isset($_GET['sup'])) {
     unset($_SESSION['panier']);
 }
@@ -12,11 +11,22 @@ if (!isset($_SESSION['quantitePlat'])) {
 ?>
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap');
+    svg {
+        display: inline-block; 
+        width: 1em;
+        height: 1em;
+        font-family: inherit;
+        font-size: inherit;
+        color: inherit;
+        vertical-align: -0.125em;
+        fill: currentColor;
+    }
+
 </style>
 
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="./index.php?action=accueil">
+        <a class="navbar-brand" href="./">
             <img src="./ressources/roundLogoLS.png" alt="logo" width="100" height="100" class="d-inline-block align-top">
             <div style="display: inline; font-size: 3em; font-family: 'Dancing Script', cursive;"><i>Baratie</i></div>
         </a>
@@ -26,12 +36,23 @@ if (!isset($_SESSION['quantitePlat'])) {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="font-size: 20px;">
                 <li class="nav-item">
-                    <a class="nav-link" name="choice" value="home" href="./index.php?action=accueil">Accueil</a>
+                    <a class="nav-link" name="choice" value="home" href="./">
+                        Accueil
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                            <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                        </svg>
+                    </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" name="choice" value="menu" href="./index.php?action=menu">Menu</a>
+                    <a class="nav-link" name="choice" value="menu" href="./index.php?action=menu">
+                        Menu
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                        </svg>
+                    </a>
                 </li>
-                <?php if (!isset($_SESSION['isLoggedIn'])) : ?>
+                <?php if (!isset($_SESSION['isLoggedIn'])) : header_remove();?>
                     <li class="nav-item">
                         <a class="nav-link" name="choice" value="connexion" href="./index.php?action=connexion&accountExists=false">Inscription</a>
                     </li>
@@ -49,10 +70,21 @@ if (!isset($_SESSION['quantitePlat'])) {
                                 <a class="dropdown-item" href="./index.php?action=userPage">Profile</a>
                             </li>
                             <li class="nav-item">
-                                <a class="dropdown-item" href="./index.php?action=panier">Panier</a>
+                                <a class="dropdown-item" href="./index.php?action=panier">
+                                    Panier
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket" viewBox="0 0 16 16">
+                                        <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1v4.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 13.5V9a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h1.217L5.07 1.243a.5.5 0 0 1 .686-.172zM2 9v4.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V9H2zM1 7v1h14V7H1zm3 3a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 4 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 6 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3A.5.5 0 0 1 8 10zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5zm2 0a.5.5 0 0 1 .5.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 1 .5-.5z" />
+                                    </svg>
+                                </a>
                             </li>
                             <li class="nav-item">
-                                <a class="dropdown-item" href="./index.php?action=logout">Se déconnecter</a>
+                                <a class="dropdown-item" href="./index.php?action=logout">
+                                    Se déconnecter
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
+                                        <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+                                    </svg>
+                                </a>
                             </li>
                         </ul>
                     </li>
